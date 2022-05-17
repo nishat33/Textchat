408c93e50f2ad6dd755cf60c133cccb775dd8a35
nishat33 <n.t.sithy@gmail.com>
2022-05-18 01:12:06 +0600

    comitting again

---
+++ app/src/main/java/com/BranchFile.java
@@ -0,0 +1,18 @@
+public class NewFile {
+    private int roll;
+    private int cg;
+
+    public int getRoll
+    {
+        return roll;
+    }
+
+    public void setRoll(int r) {
+        this.roll = r;
+    }
+
+    public NewFile (int rr){
+        this.roll=r;
+    }
+}
+
